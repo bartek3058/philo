036b2c06326f46bf30a1dcce1bfee2f45ad391d3
bartek3058 <bartek3058@gmail.com>
2025-03-04 21:59:31 +0100

    1

---
+++ philo/main.c
@@ -0,0 +1,69 @@
+#include "philo.h"
+
+void init_forks(pthread_mutex_t *forks, int philo_num)
+{
+    int i;
+
+    i = 0;
+    while (i < philo_num)
+    {
+        pthread_mutex_init(&forks[i], NULL);
+        i++;
+    }
+}
+
+void init_program(t_program *program, t_philo *philos)
+{
+    program->dead_flag = 0;
+    program->philos = philos;
+    pthread_mutex_init(&program->write_lock, NULL);
+    pthread_mutex_init(&program->dead_lock, NULL);
+    pthread_mutex_init(&program->meal_lock, NULL);
+}
+
+int check_arg_number(char *str)
+{
+    int i;
+
+    i = 0;
+    while(str[i] != '\0')
+    {
+        if(!(str[i] >= '0' && str[i] <= '9'))
+            return (1);
+        i++;
+    }
+    return (0);
+}
+
+int check_valid_args(char **argv)
+{
+    if(ft_atoi(argv[1]) > PHILO_MAX || ft_atoi(argv[1]) <= 0
+            || check_arg_number(argv[1]) == 1)
+            return (write(2, "Invalid philosophers number\n", 29), 1);
+    if(ft_atoi(argv[2]) <= 0 || check_arg_number(argv[2]) == 1)
+            return (write(2, "Invalid time to die\n", 21), 1);
+    if(ft_atoi(argv[3]) <= 0 || check_arg_number(argv[3]) == 1)
+            return (write(2, "Invalid time to eat\n", 21), 1);
+    if(ft_atoi(argv[4]) <= 0 || check_arg_number(argv[4]) == 1)
+            return (write(2, "Invalid time to sleep\n", 23), 1);
+    if(argv[5] && (ft_atoi(argv[5]) <= 0 
+            || check_arg_number(argv[5]) == 1))
+            return (write(2, "Invalid number philo must eat\n", 30), 1);
+    return (0);
+}
+
+int main(int argc, char **argv)
+{
+    t_program   program;
+    t_philo     philos[PHILO_MAX];
+    pthread_mutex_t forks[PHILO_MAX];
+
+    if(argc != 5 && argc != 6)
+        return(write(2, "Wrong argument count\n", 22), 1);
+    if (check_valid_args(argv) == 1)
+        return(1);
+    init_program(&program, philos);
+    init forks(forks, ft_atoi(argv[1]));
+    init_philo(philos, &program, forks, argv);
+    thread_create(&program, forks);
+}
---
+++ philo/philo.h
@@ -0,0 +1,44 @@
+#ifndef PHILO_H
+# define PHILO_H
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <unistd.h>
+
+# define PHILO_MAX 300
+
+typedef struct s_philo
+{
+    pthread_t       thread;
+    int                     id;
+    int                     eating;
+    int                     meal_eaten;
+    size_t          last_meal;
+    size_t          time_to_die;
+    size_t          time_to_eat;
+    size_t          time_to_sleep;
+    size_t          start_time;
+    int                     num_of_philos;
+    int                     num_times_to_eat;
+    int                     *dead;
+    pthread_mutex_t *r_fork;
+    pthread_mutex_t *l_fork;
+    pthread_mutex_t *write_lock;
+    pthread_mutex_t *dead_lock;
+    pthread_mutex_t *meal_lock;
+}                               t_philo;
+typedef struct s_program
+{
+    int                         dead_flag;
+    pthread_mutex_t dead_lock;
+    pthread_mutex_t meal_lock;
+    pthread_mutex_t write_lock;
+    t_philo                 *philos;
+}                                   t_program;
+
+int main(int argc, char **argv);
+int check_valid_args(char **argv);
+int check_arg_number(char *str);
+void init_philo(t_philo *philos, t_program *program, pthread_mutex_t *forks, char **argv);
+size_t  get_current_time(void);
